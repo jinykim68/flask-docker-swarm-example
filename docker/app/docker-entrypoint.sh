a3b7416b34f202df86f881a9c6ac8ff475badc8b
Jinyoung Kim <jinyoung.kim@unimelb.edu.au>
2019-10-30 14:07:04 +1100

    Added docker-compose.yml for nginx (SWARM mode NOT working); Updated app/docker-entrypoint.sh for bind option.

--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 target=${1-:app}
+bind=${2-:0.0.0.0:5000}
 
 printf "Running ${target} ...\n"
 
 if [[ "${target}" == app-celery-worker ]] ; then
     exec celery worker -A app.server:celery -P gevent --concurrency=1000 --loglevel=info
 else
-    exec gunicorn app.server:app --bind 0.0.0.0:5000 --timeout 180 --worker-class geventwebsocket.gunicorn.workers.GeventWebSocketWorker --worker-connections 1000 --log-level=debug
+    exec gunicorn app.server:app --bind ${bind} --timeout 180 --worker-class geventwebsocket.gunicorn.workers.GeventWebSocketWorker --worker-connections 1000 --log-level=debug
 fi
